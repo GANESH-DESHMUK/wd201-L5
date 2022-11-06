e297f3fb8422bc6efcd0aa7ee4fc0187a586276c
GANESH-DESHMUK <dganesh@vardhaman.org>
2022-11-06 07:15:04 +0530

    addTask to insert a row into table

---
+++ index.js
@@ -0,0 +1,20 @@
+const { connect } = require("./connectDB.js");
+const Todo = require("./TodoModel.js");
+
+const createTodo = async () => {
+  try {
+    await connect();
+    const todo = await Todo.addTask({
+      title: "Second Item",
+      dueDate: new Date(),
+      completed: false,
+    });
+    console.log(`Created todo with ID: ${todo.id}`);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+(async () => {
+  await createTodo();
+})();
